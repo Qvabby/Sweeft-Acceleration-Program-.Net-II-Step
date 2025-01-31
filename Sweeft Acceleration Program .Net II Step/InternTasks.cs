62a015c1a227ad3e7db997b2d14c51d3cc8645c2
Saba Salukvadze <saluqvadze2006@gmail.com>
2025-01-31 15:18:27 +0400

    IsPalindrome (works) MinSplit (works)

---
+++ Sweeft Acceleration Program .Net II Step/InternTasks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweeft_Acceleration_Program_.Net_II_Step
+{
+    public class InternTasks
+    {
+        //არის თუ არა ტექსტი პალინდრომი
+        /// <summary>
+        /// Checks if specific string is Palindrome or not.
+        /// </summary>
+        /// <param name="s">String you want to check.</param>
+        /// <returns>Boolean value.</returns>
+        public bool IsPalindrome(string s)
+        {
+            //1st method
+            //Reversing the string
+            //string reverse = "";
+            //for (int j = s.Length - 1; j >= 0; j--)
+            //{
+            //    reverse += s[j];
+            //}
+            //checking if Reversed string is same as the original and returning boolean value.
+            //return reverse == s;
+
+            //2nd method.
+            for (int i = 0; i < s.Length/2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+        //აბრუნებს მონეტების მინიმალურ რაოდენობას, რომლითაც შეგვიძლია ეს თანხა დავახურდაოთ
+        /// <summary>
+        /// Returns the minimal amount of coins for specific amount of Tetri.
+        /// </summary>
+        /// <param name="amount">Amount of Tetri</param>
+        /// <returns>Amount of coins</returns>
+        public int MinSplit(int amount)
+        {
+            //Instead of using loops we can use this method.
+            //1 5 10 20 50
+            int x = amount / 50;
+            amount -= x*50;
+            int y = amount / 20;
+            amount -= y * 20;
+            int z = amount / 10;
+            amount -= z * 10;
+            int m = amount / 5;
+            amount -= m * 5;
+            int n = amount;
+            amount -= n * 1;
+            return x+y+z+m+n;
+        }
+    }
+}
--- Sweeft Acceleration Program .Net II Step/Program.cs
+++ Sweeft Acceleration Program .Net II Step/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using Sweeft_Acceleration_Program_.Net_II_Step;
+
+InternTasks internTasks = new InternTasks();
+
+//Task 1. (IsPalindrome function)
+Console.WriteLine("Checking IsPalindrome function:");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\tmalayalam - {internTasks.IsPalindrome("malayalam")}");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"\tmala - {internTasks.IsPalindrome("mala")}\n\n");
+Console.ResetColor();
+
+
+//Task 2. (MinSplit function)
+Console.WriteLine("Check Minimal amount of coins for Tetrebi:");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine($"\t4 Tetri - {internTasks.MinSplit(4)} coins minimal amount");
+Console.WriteLine($"\t40 Tetri - {internTasks.MinSplit(40)} coins minimal amount");
+Console.WriteLine($"\t60 Tetri - {internTasks.MinSplit(60)} coins minimal amount");
+Console.ResetColor();   
